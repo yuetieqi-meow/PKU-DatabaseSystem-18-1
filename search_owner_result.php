a56686994936df0919d1947c6da3c3d79cdc2a8e
yuetieqi-meow <44973544+yuetieqi-meow@users.noreply.github.com>
2018-11-27 14:50:54 +0800

    Add files via upload

--- search_owner_result.php
+++ search_owner_result.php
@@ -6,6 +6,31 @@
 </head>
 <body>
 	<style type="text/css">
+		ul.upper_navigation{
+			list-style-type: none;
+			margin: 0;
+			padding: 10px;
+			background-color: gray;
+		}
+		li{
+			display: inline;
+			margin: 0px;
+		}
+		li.current_navigation a{
+			background-color: #111;
+		}
+		li.upper_navigation a,li.current_navigation a{
+			color: white;
+			text-align: center;
+			text-decoration: none;
+			padding: 10px;
+		}
+		li.upper_navigation a:hover{
+			background-color: #111;
+		}
+		li.upper_navigation a:visited{
+			color: white;
+		}
 		div.content_odd{
 			background-color: DarkGray;
 			padding: 5px;
@@ -33,7 +58,14 @@
 	<div id="header" style="background-color: orange; padding: 5px;" >
 		<h2 style="text-align: center;">根据书名查询拥有的人 · 检索结果</h2>		
 	</div>
-
+	<ul class="upper_navigation">
+		<li class="upper_navigation"><a href="search_by_name">书目检索</a></li>
+		<li class="current_navigation"><a href="search_owner">作者检索</a></li>
+		<li class="upper_navigation"><a href="search_sales">销量检索</a></li>
+		<li class="upper_navigation"><a href="boolean_search">布尔检索</a></li>
+		<li class="upper_navigation" style="float: right;"><a href="admin_login">管理员登陆</a></li>
+		
+	</ul>
 
 	<?php
 		error_reporting(0);
@@ -47,7 +79,7 @@
 		$mysqli = new mysqli('localhost', $username, $password, $sqlname);
 		
 		//解决中文显示成问号的问题
-//		$mysqli->query('set names utf8') or die('query字符集错误');	
+		$mysqli->query('set names utf8') or die('query字符集错误');	
 
 		//执行SQL语句
 		$sql_query = "SELECT * FROM member WHERE mID In (select wowner from warehouse where wbook in (SELECT bISBN FROM bookall WHERE bname = '".$bookname."'));";
