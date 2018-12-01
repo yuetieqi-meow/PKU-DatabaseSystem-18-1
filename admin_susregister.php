c83795023162fcf7b5b77c024a2f586cf77e79cb
cuencaa <45045365+cuencaa@users.noreply.github.com>
2018-12-01 15:09:15 +0800

    上传的时候漏掉了注册功能

---
+++ admin_susregister.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>注册成功</title>
+</head>
+<body>
+	<style type="text/css">
+		div.content_odd{
+			background-color: AliceBlue;
+			padding: 5px;
+		}
+		div.content_even{
+			background-color: AliceBlue;
+			padding: 5px;
+		}
+		div:link{}
+		div:hover{
+			background-color: Aqua;
+		}
+		a {font-size:16px
+			text-align:center;
+			} 
+		a:link {color: blue; text-decoration:none;} //未访问：蓝色、无下划线 
+		a:hover {background-color: Aqua; text-decoration:underline;} //鼠标移近：红色、下划线 
+		h3{
+			text-align: center;
+		}
+
+	</style>
+	<script language=JavaScript></script>
+
+	<div id="header" style="background-color: orange; padding: 5px;" >
+		<h2 style="text-align: center;">注册</h2>		
+	</div>
+	
+
+	<?php
+ 	$username = $_POST['name'];
+	$userpd = $_POST['password'];
+	$userpd2 = $_POST['password2'];
+
+		echo '<h3>你好</h3>';
+
+		//连接数据库
+		$mysqli = new mysqli('localhost', 'root', '', 'booksql');
+		
+		if(!$mysqli){
+			die("连接数据库失败：".mysqli_error());
+		}
+		
+		//$user_query = "SELECT * FROM user_login_data WHERE username= $username ";
+		
+		if (empty($_POST["name"])) {
+			
+		echo "<h3>错误：姓名不能为空。</h3><a href='javascript:history.back(-1);'><h3>返回</h3></a>";
+			exit;
+		} 
+		
+
+		if (empty($_POST["password"])) {
+		echo '错误：密码是必填的。<a href="javascript:history.back(-1);">返回</a>';
+			exit;
+		} else {
+		$password = test_input($_POST["password"]);
+		}
+
+		if (empty($_POST["password2"])) {
+		echo '错误：必须确认密码。<a href="javascript:history.back(-1);">返回</a>';
+			exit;
+		} else {
+		$password2 = test_input($_POST["password2"]);
+		}if ($_POST["password"] != $_POST["password2"]) {
+		echo '错误：两次输入的密码不一致。<a href="javascript:history.back(-1);">返回</a>';
+			exit;
+		}
+		
+		function test_input($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
+		
+		
+		//检测用户名是否已存在
+		$check_query = mysqli_query($mysqli, 'SELECT * FROM user_login_data WHERE username="'. $username. '"' );
+		if (mysqli_fetch_array($check_query)){
+			echo '错误：用户名已存在。<a href="javascript:history.back(-1);">返回</a>';
+			exit;
+		}
+		
+		
+		//解决中文显示成问号的问题
+		$mysqli->query('set names utf8') or die('query字符集错误');	
+
+		//在数据库中添加
+		$sql_query = "INSERT INTO user_login_data(username, password) VALUES('$username','$userpd')";
+		if(mysqli_query($mysqli,$sql_query )){
+				exit('用户注册成功！点击此处<a href ="admin_login.php">登录</a>');
+			
+		}
+		else{
+			echo '添加数据失败：'.mysqli_error(),'<br />';
+			echo '点击此处 <a href="javascript:history.back(-1);">返回</a> 重试';
+		}
+		
+	?>
+	
+	<br>
+	<br>
+	<form action="search_owner.php">
+		<input type="submit" id="submit" value="继续注册">
+	</form>
+
+</body>
+</html>
---
+++ admin_userregister.php
@@ -0,0 +1,98 @@
+﻿
+<!DOCTYPE HTML> 
+<html>
+<head>
+<title>用户注册界面</title>
+<style>
+.error {color: #FF0000;}
+	h2{
+			background-color: AliceBlue;
+			padding: 10px;
+			text-align:center;
+		}
+		
+	h3{
+		text-align:center;
+		padding:5px 20px;
+		color: #FF0000;
+	}
+	
+</style>
+
+</head>
+<body> 
+
+<?php
+// 定义变量并设置为空值
+$nameErr = $passwordErr =  $password2Err = "";
+$name = $password = $password2 = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["name"])) {
+     $nameErr = "姓名是必填的";
+   } else {
+     $name = test_input($_POST["name"]);
+	 //检查姓名是否包含字母和空白字符
+	 if (!preg_match("/^[a-zA-Z ]*$/",$name)){
+	   $nameErr = "只允许字母和空格";
+		}
+   }
+
+   if (empty($_POST["password"])) {
+     $password = "";
+   } else {
+     $password = test_input($_POST["password"]);
+   }
+
+   if (empty($_POST["password2"])) {
+     $password2 = "";
+   } else {
+     $password2 = test_input($_POST["password2"]);
+	  if ($_POST["password"] != $_POST["password2"]) {
+     $password2Err = "两次输入的密码不一致";
+   }
+   }
+ 
+}
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+?>
+
+<h2>用户注册</h2>
+<h3>* 必需的字段</h3>
+
+<form method="post" action="admin_susregister.php">
+ 
+	<div style="display: flex; justify-content: center;">
+   姓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;名：<input type="text" name="name">
+   <span class="error">* <?php echo $nameErr;?></span>
+   </div>
+   <br><br>
+   
+   <div style="display: flex; justify-content: center;">
+   密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" name="password">
+   <span class="error">* <?php echo $passwordErr;?></span>
+   </div>
+   <br><br>
+   
+   <div style="display: flex; justify-content: center;">
+   确认密码：<input type="password" name="password2">
+   <span class="error">* <?php echo $password2Err;?></span>
+   </div>
+   <br><br>
+   
+   <div style="display: flex; justify-content: center;">
+   <input type="submit" name="submit" value="注册"> 
+   </div>
+   
+</form>
+
+
+
+</body>
+</html>
