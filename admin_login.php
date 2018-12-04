0a2440cc5b587bd7e26379ccd5e794064c8f3940
cuencaa <45045365+cuencaa@users.noreply.github.com>
2018-12-04 14:22:20 +0800

    根据手机号码登录

--- admin_login.php
+++ admin_login.php
@@ -24,19 +24,19 @@
 </head>
 <body> 
 
-<h2>管理员登录</h2>
+<h2>登录</h2>
 <h3>* 必需的字段</h3>
 
 <form name="form" onsubmit="return validateForm()" method="post" action="admin_login_verification.php" >
    
    <div style="display: flex; justify-content: center;">
-   姓名：<input type="text" name="admin_username">
+   手机号码：<input type="text" name="admin_username">
    <span id="username_span" class="error">* </span>
    </div>
    <br><br>
    
    <div style="display: flex; justify-content: center;">
-   密码：<input type="password" name="admin_password">
+   密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" name="admin_password">
    <span id="password_span" class="error">* </span>
    </div>
    <br><br>
@@ -48,8 +48,8 @@
 
 <?php 
    if(isset($_POST['error_type'])){
-      if($_POST['error_type'] == 'admin_username_not_found'){
-         echo '<span class="error"> · 用户名不存在！</span>';    
+      if($_POST['error_type'] == 'admin_phone_not_found'){
+         echo '<span class="error"> · 手机号码不存在！</span>';    
       }
       else if($_POST['error_type'] == 'admin_password_incorrect'){
          echo '<span class="error"> · 密码错误！</span>';
@@ -61,10 +61,10 @@
    function validateForm(){
       /*验证表单是否填充完全*/
       var legal = 1;    //合法性变量，如果存在不合法的情况就将其改为0
-      var username = document.forms["form"]["username"].value;
+      var phone = document.forms["form"]["phone"].value;
       var password = document.forms["form"]["password"].value;
-      if(username == ""){
-         document.getElementById('username_span').innerHTML = "* 用户名不能为空";
+      if(phone == ""){
+         document.getElementById('username_span').innerHTML = "* 手机号不能为空";
          legal = 0;
       }
       else{
