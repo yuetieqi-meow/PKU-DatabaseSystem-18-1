97f11bd47bf6c1bbb168f59167a83bff33df27ca
yuetieqi-meow <44973544+yuetieqi-meow@users.noreply.github.com>
2018-12-25 14:10:53 +0800

    Add files via upload

--- messagesubmit.php
+++ messagesubmit.php
@@ -9,7 +9,6 @@
 <body>
 <?php
 $message = $_POST['message'];
-$customer = $COOKIE['customer_phone'];
 $t = time();
 $time = date("Y-m-d", $t);
 
@@ -17,11 +16,14 @@ $username = $_COOKIE['username'];
 $password = $_COOKIE['password'];
 $sqlname = $_COOKIE['sqlname'];
 //连接数据库
+
 $mysqli = new mysqli('localhost', $username, $password, $sqlname);
 $number_sql = "SELECT COUNT(*) FROM comment";
 list($number) = $mysqli->query($number_sql, MYSQLI_STORE_RESULT)->fetch_row();
 $number = (int)$number + 1;
-
+$customer_phone = $_COOKIE['customer_phone'];
+$search_user_sql='SELECT cID FROM customer where cphone="'.$customer_phone.'"';
+list($customer)=$mysqli->query($search_user_sql)->fetch_row();
 $sql = 'INSERT INTO comment(cmtID, cmtcustomer, cmttime, cmtmessage,cmtreplystatus) VALUES("' . $number . '","' . $customer . '","' . $time . '","' . $message . '",0)';
 $mysqli->query($sql);
 header("Location: leavemessage.php");
